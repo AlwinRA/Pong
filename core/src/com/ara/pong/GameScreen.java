9bfaf135dd500cacd7cf1a89f64a115ac6b0e75a
Alwin <93728357+Alwinjesus@users.noreply.github.com>
2022-06-12 19:20:08 +0530

    init commit

---
+++ core/src/com/ara/pong/GameScreen.java
@@ -0,0 +1,24 @@
+package com.ara.pong;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class GameScreen extends ScreenAdapter {
+
+	@Override
+	public void show () {
+	}
+
+    @Override
+	public void render (float delta) {
+        ScreenUtils.clear(0, 0, 0.2f, 1);
+		Gdx.app.log("GameScreen", "Render");
+	}
+
+	@Override
+	public void dispose () {
+        Gdx.app.log("GameScreen", "Dispose");
+	}
+
+}
--- core/src/com/ara/pong/Pong.java
+++ core/src/com/ara/pong/Pong.java
@@ -1,31 +1,23 @@
 package com.ara.pong;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 
-public class Pong extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+public class Pong extends Game {
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		this.setScreen(new GameScreen());
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		super.render();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
+		Gdx.app.log("Pong", "Dispose");
+		this.getScreen().dispose();
 	}
 }
